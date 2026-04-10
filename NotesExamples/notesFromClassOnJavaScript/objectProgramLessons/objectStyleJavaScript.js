43e4b6ecbb8950634f8a9970b09cfe2d99c1103c
FluffyD3rd <sparecount79@gmail.com>
2026-04-09 21:08:44 -0400

    notes from class

---
+++ NotesExamples/notesFromClassOnJavaScript/objectProgramLessons/objectStyleJavaScript.js
@@ -0,0 +1,15 @@
+document.querySelector('#id1').addEventListener('mouseover',function(){
+    this.style.color = "green";
+});
+
+document.querySelector('#id1').addEventListener('mouseout', function(){
+    this.style.color = "black";
+});
+
+document.querySelector('#id1').onclick=function(){
+    this.textContent = "Hello";
+}
+
+window.onresize = function(){
+    document.body.style.backgroundColor = 'yellow';
+}
