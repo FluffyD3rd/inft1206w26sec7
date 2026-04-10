80477348ef42b97a54a2dd84a0b57c9d46c090b3
FluffyD3rd <sparecount79@gmail.com>
2026-04-10 14:12:47 -0400

    add description/ comment header

--- Assignment4/Part2/imageGalleryScript.js
+++ Assignment4/Part2/imageGalleryScript.js
@@ -1,5 +1,12 @@
+/*
+Name: Nathaniel Shearing
+  File: sillyStoryScript
+  Date: 09 April 2026
+  Javascript file that is linked with the imageGallery html file.
+  This file contains the javascript code that controls the interactions
+  on the html page. 
 
-
+*/
 
 
 const displayedImage = document.querySelector(".displayed-img");
@@ -7,3 +14,17 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+const images = [
+    {filename: "pic1.jpg", alt:"Closeup of a human eye."},
+    {filename: "pic2.jpg", alt: "Rock that looks like a wave"},
+    {filename: "pic3.jpg", alt: "Purple and white pansies"},
+    {filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb"},
+    {filename: "pic5.jpg", alt: "Large moth on a leaf"}
+];
+
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+for (const photo of images){
+    console.log(value);
+}
