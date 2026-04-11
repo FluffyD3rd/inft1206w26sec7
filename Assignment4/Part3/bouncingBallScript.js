bfed10cbda1e9e194607ec83937af41e494c854b
FluffyD3rd <sparecount79@gmail.com>
2026-04-10 21:08:55 -0400

    add constants

--- Assignment4/Part3/bouncingBallScript.js
+++ Assignment4/Part3/bouncingBallScript.js
@@ -8,4 +8,10 @@ Name: Nathaniel Shearing
 
 */
 
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
 
