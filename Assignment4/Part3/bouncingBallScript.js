5a062413a688989e50b363e54bdf00361445e13d
FluffyD3rd <sparecount79@gmail.com>
2026-04-10 21:01:28 -0400

    create file and add description

---
+++ Assignment4/Part3/bouncingBallScript.js
@@ -0,0 +1,11 @@
+/*
+Name: Nathaniel Shearing
+  File: bouncingBallScript
+  Date: 09 April 2026
+  Javascript file that is linked with the imageGallery html file.
+  This file contains the javascript code that controls the interactions
+  on the html page. 
+
+*/
+
+
