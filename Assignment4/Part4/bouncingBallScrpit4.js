f14bc23244624d4743a97031d926f5e37d9f4d0a
FluffyD3rd <sparecount79@gmail.com>
2026-04-10 22:11:54 -0400

    add updates to script

--- Assignment4/Part4/bouncingBallScrpit4.js
+++ Assignment4/Part4/bouncingBallScrpit4.js
@@ -81,8 +81,64 @@ class Ball extends Shape{
 class evilCircle extends Shape{
     constructor(x,y){
         super(x,y,20,20);
+        this.color = "white";
+        this.size = 10;
+        window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
     }
+        draw() {
+    ctx.beginPath();
+    ctx.linewidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
 }
+checkBounds(){
+    if (this.x + this.size >= width) {
+      this.x = -this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x = -this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y = -this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y = -this.size;
+    }
+  }
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+        }
+      }
+    }
+  }
+}
+
 
 const balls = [];
 
@@ -102,14 +158,22 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 function loop() {
+    const evilCircle1 = new evilCircle(15,15);
+
+
   ctx.fillStyle = "rgb(0 0 0 / 25%)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
+    if (ball.exists){
     ball.draw();
     ball.update();
     ball.collisionDetect();
   }
+    evilCircle1.draw();
+    evilCircle1.checkBounds();
+    evilCircle1.collisionDetect();
+}
 
   requestAnimationFrame(loop);
 }
