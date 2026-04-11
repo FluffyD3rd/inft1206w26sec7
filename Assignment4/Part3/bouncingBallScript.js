c362cbe96837eeaf0de79dfa64a37a20825e04cf
FluffyD3rd <sparecount79@gmail.com>
2026-04-10 21:04:36 -0400

    update descriptions

--- Assignment4/Part3/bouncingBallScript.js
+++ Assignment4/Part3/bouncingBallScript.js
@@ -2,9 +2,9 @@
 Name: Nathaniel Shearing
   File: bouncingBallScript
   Date: 09 April 2026
-  Javascript file that is linked with the imageGallery html file.
-  This file contains the javascript code that controls the interactions
-  on the html page. 
+  Javascript file that is linked with the bouncingBall html file. 
+  This file contains the javascript code for the bouncing ball page.
+  It contains the code for the creation and "bouncing" of the balls.
 
 */
 
